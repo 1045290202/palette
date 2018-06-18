0b10c69fc6475c035a65b17fef77ae1d62c1553c
1045290202 <1045290202@qq.com>
2018-06-18 10:48:05 +0800

    ...

--- app/src/main/java/com/sjk/palette/CustomDialog.java
+++ app/src/main/java/com/sjk/palette/CustomDialog.java
@@ -9,6 +9,11 @@ public abstract class CustomDialog extends PopupWindow {
 
     public abstract void dialogInit(Context context);
 
+    /**
+     * 关闭弹出框，由取消按钮调用
+     *
+     * @param delayMills 延迟时间
+     */
     public void delayAndDismiss(int delayMills) {
         Handler handle = new Handler();
         handle.postDelayed(new Runnable() {
